c220c9c9ac6d6fe97014b88c32cc01789e226685
Mike <ilnitsky@ngs.ru>
2019-04-04 01:19:12 +0700

    Task11_Lodash: реализовано простой заменой Underscore на Lodash

---
+++ Task11_Lodash/script.js
@@ -0,0 +1,54 @@
+"use strict";
+
+(function () {
+    var people = [
+        {name: "Михаил", lastName: "Михайлов", age: 99},
+        {name: "Валентин", lastName: "Валентинов", age: 89},
+        {name: "Николай", lastName: "Николаев", age: 79},
+        {name: "Сидор", lastName: "Сидоров", age: 39},
+        {name: "Пётр", lastName: "Петров", age: 29},
+        {name: "Анна", lastName: "Аннова", age: 27},
+        {name: "Алексей", lastName: "Алексеев", age: 49},
+        {name: "Дамир", lastName: "Дамиров", age: 59},
+        {name: "Василиса", lastName: "Васильева", age: 25},
+        {name: "Серафима", lastName: "Серафимова", age: 21},
+        {name: "Юрий", lastName: "Юрьев", age: 17},
+        {name: "Илья", lastName: "Ильин", age: 23}
+    ];
+
+    function print(arrayOfObjects) {
+        arrayOfObjects.forEach(function (obj, i) {
+            for (var property in obj) {
+                console.log("[" + i + "] " + property + " : " + obj[property]);
+            }
+        });
+    }
+
+    console.log("Исходный массив people:");
+    print(people);
+    console.log();
+
+    people = _.chain(people).each(function (p) {
+        p.fullName = p.lastName + " " + p.name;
+    }).value();
+    console.log("Массив people с полным именем:");
+    print(people);
+    console.log();
+
+    var averageAge = _.chain(people)
+        .reduce(function (memo, p) {
+            return memo + p.age;
+        }, 0)
+        .value() / people.length;
+    console.log("Средний возраст:" + averageAge);
+    console.log();
+
+    var peopleFrom20To30 = _.chain(people)
+        .filter(function (p) {
+            return p.age >= 20 && p.age <= 30;
+        })
+        .sortBy("age")
+        .value();
+    console.log("Массив people от 20 до 30 с сортировкой по имени:");
+    print(peopleFrom20To30);
+})();
